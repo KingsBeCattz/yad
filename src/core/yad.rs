7b365b4658c351514b89fb4bb5ccf6cef6d067b1
KingsBeCattz <jdanielxd.3@gmail.com>
2025-08-21 10:38:35 -0700

    feat(YAD): Improved YAD struct usage. Added implement and related functions

--- src/core/yad.rs
+++ src/core/yad.rs
@@ -8,6 +8,23 @@ pub struct YAD {
     pub rows: HashMap<String, Row>
 }
 
+impl YAD {
+    pub fn new() -> Self {
+        Self {
+            version: YAD_CURRENT_VERSION,
+            rows: HashMap::new()
+        }
+    }
+
+    pub fn add_row(&mut self, row: Row) {
+        self.rows.insert(row.name.clone(), row);
+    }
+
+    pub fn get_row(&self, key: &str) -> Option<&Row> {
+        self.rows.get(key)
+    }
+}
+
 impl AsRef<YAD> for YAD {
     fn as_ref(&self) -> &YAD {
         &self
