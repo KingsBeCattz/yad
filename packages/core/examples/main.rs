51ee16512a31edb1cecf91705316f23d91a0fdcb
KingsBeCattz <jdanielxd.3@gmail.com>
2025-08-26 14:13:37 -0700

    refactor(examples): update main.rs to use new Row key management methods

--- packages/core/examples/main.rs
+++ packages/core/examples/main.rs
@@ -6,19 +6,17 @@ fn main() {
     let mut yad = YAD::new();
 
     let mut johan_user = Row::new("johan".to_string(), HashMap::new());
-    johan_user.keys.insert(
-        "name".to_string(),
+    johan_user.add_key(
         Key::new("name".to_string(), Value::from_string("Johan".to_string()).unwrap())
     );
-    johan_user.keys.insert(
-        "age".to_string(),
+    johan_user.add_key(
         Key::new("age".to_string(), Value::from_u8(17))
     );
-    let johan_proyects = vec![Value::from_string("Yad".to_string()).unwrap()];
 
-    johan_user.keys.insert(
-        "projects".to_string(),
-        Key::new("projects".to_string(), Value::from_vec(johan_proyects).unwrap())
+    johan_user.add_key(
+        Key::new("projects".to_string(), Value::from_vec(vec![
+            Value::from_string("Yad".to_string()).unwrap()
+        ]).unwrap())
     );
 
     yad.add_row(johan_user);
